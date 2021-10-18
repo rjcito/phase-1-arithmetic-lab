478829baea10447679dd519ce95982b50a51fb45
Roberto Jacquez <rjqz92@gmail.com>
2021-10-18 15:18:28 -0700

    commit tree 3d0be6f40ea2f2d6f4291c29b56551906bfce51a

--- index.js
+++ index.js
@@ -1,2 +1,13 @@
+const num1 = 2;
+const num2 = 31;
+let multiply = num1*num2;
 
-// Write your code here
+let random;
+random = Math.ceil((Math.random()*10));
+
+const num3 = 19; 
+const num4 = 5;
+let mod = 19%5;
+
+let max;
+max = Math.max(1,2,3,4,5,6,7,8,9,10,11,12,13,20);
